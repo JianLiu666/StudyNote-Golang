1ab1049a27342e6cd9c4a72ec1f983d2f9113c6b
jian_liu <jian_liu@paradise-soft.com.tw>
2020-04-23 20:43:37 +0800

    add websocket note

---
+++ websocket/2_ws_ulimit/server.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync/atomic"
+	"syscall"
+
+	"github.com/gorilla/websocket"
+)
+
+var count int64
+
+func ws(w http.ResponseWriter, r *http.Request) {
+	// Upgradge connection
+	upgrader := websocket.Upgrader{}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+
+	n := atomic.AddInt64(&count, 1)
+	if n%100 == 0 {
+		log.Printf("Total number of connection: %v", n)
+	}
+	defer func() {
+		n := atomic.AddInt64(&count, -1)
+		if n%100 == 0 {
+			log.Printf("Total number of connection: %v", n)
+		}
+		conn.Close()
+	}()
+
+	// Read message from socket
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
+		log.Printf("msg: %s", string(msg))
+	}
+}
+
+func main() {
+	// Increase resources limitations
+	var rLimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+	rLimit.Cur = rLimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+
+	// Enable pprof hooks
+	go func() {
+		if err := http.ListenAndServe("localhost:6060", nil); err != nil {
+			log.Fatalf("Pprof failed: %v", err)
+		}
+	}()
+
+	http.HandleFunc("/", ws)
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
