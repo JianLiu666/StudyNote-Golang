7305dc54ba0bafb4e89048f86d9aa47660cf5218
jian_liu <jianliu0616@gmail.com>
2023-03-30 14:37:11 +0800

    feat(websocket-benchmark):  - 調整輸出方式

--- practice/websocket-benchmark/cmd/nhooyr-client/result.go
+++ practice/websocket-benchmark/cmd/nhooyr-client/result.go
@@ -2,31 +2,34 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"sort"
 
 	"github.com/sirupsen/logrus"
 )
 
 func calculate(clients []*client) {
 	var total int64
-	var shortest int64 = math.MaxInt64
-	var longest int64 = math.MinInt64
+
+	data := []int64{}
 
 	for _, client := range clients {
 		for i := 0; i < conf.Simulation.NumMessages; i++ {
 			diff := client.times[i]["client_received"] - client.times[i]["client_start"]
-			if diff < shortest {
-				shortest = diff
-			}
-			if diff > longest {
-				longest = diff
-			}
 			total += diff
+			data = append(data, diff)
 		}
 	}
 
+	sort.Slice(data, func(i, j int) bool {
+		return data[i] < data[j]
+	})
+
 	fmt.Println()
-	logrus.Infof("average timestamp: %v ms", total/(int64(conf.Simulation.NumClients*conf.Simulation.NumMessages)))
-	logrus.Infof("shortest timestamp: %v ms", shortest)
-	logrus.Infof("longest timestamp: %v ms", longest)
+	logrus.Info("timestmap")
+	logrus.Infof("average:  %v ms", total/(int64(conf.Simulation.NumClients*conf.Simulation.NumMessages)))
+	logrus.Infof("shortest: %v ms", data[0])
+	logrus.Infof("longest:  %v ms", data[len(data)-1])
+	logrus.Infof("p50:      %v ms", data[int(float64(len(data))*0.5)])
+	logrus.Infof("p90:      %v ms", data[int(float64(len(data))*0.9)])
+	logrus.Infof("p95:      %v ms", data[int(float64(len(data))*0.95)])
 }
