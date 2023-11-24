16d9f2d6953e5f4a3aca040bf7ba46aad42be1c1
jian_liu <jianliu0616@gmail.com>
2023-11-24 13:32:21 +0800

    test(interview-20231116):  - add redis unit test

--- practice/interview-20231116/go.mod
+++ practice/interview-20231116/go.mod
@@ -4,16 +4,19 @@ go 1.21.3
 
 require (
 	github.com/go-redis/redis/v8 v8.11.5
+	github.com/go-redis/redismock/v8 v8.11.5
 	github.com/gofiber/fiber/v2 v2.51.0
 	github.com/rs/xid v1.5.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.17.0
+	github.com/stretchr/testify v1.8.4
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/google/uuid v1.4.0 // indirect
@@ -26,6 +29,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
--- practice/interview-20231116/pkg/kvstore/redis/redis_test.go
+++ practice/interview-20231116/pkg/kvstore/redis/redis_test.go
@@ -1,7 +1,94 @@
 package redis
 
-import "testing"
+import (
+	"context"
+	"encoding/json"
+	"interview20231116/model"
+	"interview20231116/pkg/config"
+	"interview20231116/pkg/e"
+	"testing"
+
+	"github.com/go-redis/redismock/v8"
+	"github.com/stretchr/testify/assert"
+)
+
+var _redisClient redisClient
+var _redisMock redismock.ClientMock
 
 func TestMain(m *testing.M) {
+	setup()
 
+	m.Run()
+}
+
+func setup() {
+	conn, mock := redismock.NewClientMock()
+
+	_redisClient = redisClient{
+		conn: conn,
+		conf: &config.NewFromDefault().Redis,
+	}
+
+	_redisMock = mock
+}
+
+func TestGetListHead_Success(t *testing.T) {
+	listKey := "list-uuid"
+	pageKey := "page-uuid"
+
+	_redisMock.ExpectHGet(_redisClient.conf.ListCollectionName, listKey).SetVal(pageKey)
+
+	res, code := _redisClient.GetListHead(context.Background(), listKey)
+	if code != e.SUCCESS {
+		t.Error(e.GetMsg(code))
+	}
+
+	assert.Equal(t, pageKey, res)
+}
+
+func TestGetListHead_Fail_DataNotFound(t *testing.T) {
+	listKey := "list-uuid"
+
+	_redisMock.ExpectHGet(_redisClient.conf.ListCollectionName, listKey).RedisNil()
+
+	_, code := _redisClient.GetListHead(context.Background(), listKey)
+	if code != e.ERROR_DATA_NOT_FOUND {
+		t.Error(e.GetMsg(code))
+	}
+}
+
+func TestGetPage_Success(t *testing.T) {
+	page := &model.Page{
+		Key:         "page-uuid",
+		NextPageKey: "",
+		Articles:    []*model.Article{{ID: 1}, {ID: 2}},
+	}
+	pageJSON, err := json.Marshal(page)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_redisMock.ExpectGet(_redisClient.genPageKey(page.Key)).SetVal(string(pageJSON))
+
+	res, code := _redisClient.GetPage(context.Background(), page.Key)
+	if code != e.SUCCESS {
+		t.Error(e.GetMsg(code))
+	}
+
+	assert.Equal(t, page, res)
+}
+
+func TestGetPage_Fail_DataNotFund(t *testing.T) {
+	page := &model.Page{
+		Key:         "page1-uuid",
+		NextPageKey: "",
+		Articles:    []*model.Article{{ID: 1}, {ID: 2}},
+	}
+
+	_redisMock.ExpectGet(_redisClient.genPageKey(page.Key)).RedisNil()
+
+	_, code := _redisClient.GetPage(context.Background(), page.Key)
+	if code != e.ERROR_DATA_NOT_FOUND {
+		t.Error(e.GetMsg(code))
+	}
 }
