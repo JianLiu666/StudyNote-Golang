8649fc5ec00dd9fb57d8c0785ce5edea2eb355be
jian_liu <jianliu0616@gmail.com>
2023-11-30 03:25:37 +0800

    doc(interview-20231129):  - 補上 SinglePool 註解說明

--- practice/interview-20231129/pkg/singlepool/singlepool.go
+++ practice/interview-20231129/pkg/singlepool/singlepool.go
@@ -8,6 +8,14 @@ import (
 	"github.com/emirpasic/gods/maps/treemap"
 )
 
+// Single Pool
+//
+// 1. 使用 goDS 的 treemap (based on red-black tree) 作為配對池的主要資料結構:
+//   - add single person 時的時間複雜度約在 O(logn)
+//   - match 基於 iterator 操作, 時間複雜度為 O(n), 需要思考如何解決 (TODO)
+//
+// 2. 使用 hash table (lookup) 檢查重複創建的用戶:
+//   - 每次檢查的時間複雜度為 O(1)
 type singlePool struct {
 	mu sync.Mutex
 
@@ -25,6 +33,9 @@ func NewSinglePool() *singlePool {
 	}
 }
 
+// AddSinglePersonAndMatch 加入新用戶且根據配對規則進行配對與更新用戶狀態
+//
+// @param user 用戶資訊
 func (s *singlePool) AddSinglePersonAndMatch(user *model.User) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -46,6 +57,9 @@ func (s *singlePool) AddSinglePersonAndMatch(user *model.User) {
 	}
 }
 
+// RemoveSinglePerson 移除指定用戶
+//
+// @param name 用戶姓名
 func (s *singlePool) RemoveSinglePerson(name string) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -59,6 +73,18 @@ func (s *singlePool) RemoveSinglePerson(name string) {
 	delete(s.lookup, name)
 }
 
+// match 根據配對規則進行配對
+//
+//  1. 將 user 與 candidatePool 進行匹配, 只要符合規則就將雙方的約會次數(NumDates) 同時減 1
+//  2. 直到 candidatePool 的遍歷流程結束後, user 還有剩餘的約會次數時就將 user 加入到 userPool 內
+//
+// @param user 用戶資訊
+//
+// @param userPool 與用戶相同性別的配對池
+//
+// @param candidatePool 與用戶不同性別的配對池
+//
+// @param comp 配對規則 (true: 配對成功, false: 配對失敗)
 func (s *singlePool) match(user *model.User, userPool, candidatePool *treemap.Map, comp func(a, b int) bool) {
 	it := candidatePool.Iterator()
 	for it.Next() {
@@ -82,6 +108,9 @@ func (s *singlePool) match(user *model.User, userPool, candidatePool *treemap.Ma
 	}
 }
 
+// genHashKey 根據用戶資訊產生 unique key
+//
+// @param user 用戶資訊
 func (s *singlePool) genHashKey(user *model.User) string {
 	return fmt.Sprintf("%d-%s", user.Height, user.Name)
 }
