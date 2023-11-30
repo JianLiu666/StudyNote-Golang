66fddebf1659f667737162a4e89da325ebe9ec28
jian_liu <jianliu0616@gmail.com>
2023-11-30 14:59:49 +0800

    feat(interview-20231129):  - 實作 [DELETE] singles/{name} api

--- practice/interview-20231129/api/router/v1/single/delete.go
+++ practice/interview-20231129/api/router/v1/single/delete.go
@@ -1,7 +1,19 @@
 package single
 
-import "github.com/gin-gonic/gin"
+import (
+	"interview20231129/pkg/e"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (s *singleRouter) RemoveSinglePerson(c *gin.Context) {
+	name := c.Param("name")
 
+	if code := s.singlePool.RemoveSinglePerson(name); code != e.SUCCESS {
+		c.String(http.StatusBadRequest, e.GetMsg(code))
+		return
+	}
+
+	c.Status(http.StatusOK)
 }
