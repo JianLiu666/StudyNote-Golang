0bcc35194e8ec7f3f62c6b7c637b2e8ab3a4efdd
jian_liu <jianliu0616@gmail.com>
2023-12-13 18:25:10 +0800

    test(interview-20231208):  - add myql testcase

--- practice/interview-20231208/pkg/rdb/mysql/mysql_test.go
+++ practice/interview-20231208/pkg/rdb/mysql/mysql_test.go
@@ -13,8 +13,8 @@ import (
 func TestCreateOrder(t *testing.T) {
 	// TODO: remove magic number
 	dsn := "root:0@tcp(localhost:3306)/trading?charset=utf8mb4&parseTime=True&loc=Local"
-
 	client := NewMySqlClient(context.TODO(), dsn, 60, 100, 10)
+
 	client.CreateOrder(context.TODO(), &model.Order{
 		UserID:       1,
 		RoleType:     e.ROLE_BUYER,
@@ -25,4 +25,51 @@ func TestCreateOrder(t *testing.T) {
 		Status:       e.STATUS_PENDING,
 		Timestamp:    time.Now(),
 	})
+}
+
+func TestUpdateOrdersAndCreateTransactionLogs(t *testing.T) {
+	// prepare data
+	orderSet := map[int]*model.Order{
+		1: {ID: 1, Status: e.STATUS_SUCCESS},
+		2: {ID: 2, Status: e.STATUS_SUCCESS},
+	}
+	logs := []*model.TransactionLog{
+		{
+			BuyerOrderID:  1,
+			SellerOrderID: 2,
+			Price:         100,
+			Quantity:      100,
+			Timestamp:     time.Now(),
+		},
+	}
+
+	// TODO: remove magic number
+	dsn := "root:0@tcp(localhost:3306)/trading?charset=utf8mb4&parseTime=True&loc=Local"
+	client := NewMySqlClient(context.TODO(), dsn, 60, 100, 10)
+
+	// gen fake data
+	client.CreateOrder(context.TODO(), &model.Order{
+		UserID:       1,
+		RoleType:     e.ROLE_BUYER,
+		OrderType:    e.ORDER_LIMIT,
+		DurationType: e.DURATION_ROD,
+		Price:        100,
+		Quantity:     100,
+		Status:       e.STATUS_PENDING,
+		Timestamp:    time.Now(),
+	})
+
+	client.CreateOrder(context.TODO(), &model.Order{
+		UserID:       2,
+		RoleType:     e.ROLE_SELLER,
+		OrderType:    e.ORDER_LIMIT,
+		DurationType: e.DURATION_ROD,
+		Price:        100,
+		Quantity:     100,
+		Status:       e.STATUS_PENDING,
+		Timestamp:    time.Now(),
+	})
+
+	// testcase
+	client.UpdateOrdersAndCreateTransactionLogs(context.TODO(), orderSet, logs)
 }
