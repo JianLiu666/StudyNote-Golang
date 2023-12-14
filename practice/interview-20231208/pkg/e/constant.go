d9d5fadde9f70840bf8d6e3abfd35608e48b7ff0
jian_liu <jianliu0616@gmail.com>
2023-12-14 19:28:50 +0800

    feat(interview-20231208):  - add constant validation

--- practice/interview-20231208/pkg/e/constant.go
+++ practice/interview-20231208/pkg/e/constant.go
@@ -5,14 +5,24 @@ type ROLE_TYPE int // 交易角色
 const (
 	ROLE_BUYER  = iota // 買方
 	ROLE_SELLER        // 賣方
+	ROLE_END           // placeholder
 )
+
+func IsRoleTypeValid(n int) bool {
+	return n >= 0 && n < ROLE_END
+}
 
 type ORDER_TYPE int // 訂單類型
 
 const (
 	ORDER_MARKET = iota // 市價單
 	ORDER_LIMIT         // 限價單
+	ORDER_END           // placeholder
 )
+
+func IsOrderTypeValid(n int) bool {
+	return n >= 0 && n < ORDER_END
+}
 
 type DURATION_TYPE int // 有效期限
 
@@ -20,12 +30,22 @@ const (
 	DURATION_ROD = iota // 當日委託有效單 (Rest of Day)
 	DURATION_IOC        // 立即成交否則取消 (Immediate or Cancel)
 	DURATION_FOK        // 立即全部成交否則取消 (Fill or Kill)
+	DURATION_END        // placeholder
 )
+
+func IsDurationTypeValid(n int) bool {
+	return n >= 0 && n < DURATION_END
+}
 
 type ORDER_STATUS int
 
 const (
-	STATUS_PENDING = iota
-	STATUS_CANCEL
-	STATUS_SUCCESS
+	STATUS_PENDING = iota // 掛單中
+	STATUS_CANCEL         // 已取消
+	STATUS_SUCCESS        // 已成交
+	STATUS_END            // placeholder
 )
+
+func IsOrderStatusValid(n int) bool {
+	return n >= 0 && n < STATUS_END
+}
