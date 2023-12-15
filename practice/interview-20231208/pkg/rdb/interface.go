769c3ac83a006bb00a6f12e99cdbeef78d34d3f2
jian_liu <jianliu0616@gmail.com>
2023-12-15 16:51:25 +0800

    doc(interview-20231208):  - 加入 interface 註解

--- practice/interview-20231208/pkg/rdb/interface.go
+++ practice/interview-20231208/pkg/rdb/interface.go
@@ -11,13 +11,48 @@ const (
 )
 
 type RDB interface {
+	// Shutdown 提供外部調用 graceful shutdown 流程
+	//
+	// @param ctx context for tracing
 	Shutdown(ctx context.Context)
 
+	// CreateOrder 建立交易單
+	// 對 RDB 寫入交易單
+	//
+	// @param ctx context for tracing
+	//
+	// @param order 欲建立的交易單
 	CreateOrder(ctx context.Context, order *model.Order)
 
+	// GetOrders 取得交易單
+	// 根據傳入的 filters 組裝 SQL 後取回符合條件的交易單
+	//
+	// @param ctx context for tracing
+	//
+	// @param opts 查詢條件
+	//
+	// @return []*model.Order 符合條件的交易單
 	GetOrders(ctx context.Context, opts *model.OrderQueryOpts) []*model.Order
 
+	// UpdateOrdersAndCreateTransactionLogs 更新交易紀錄
+	// 事務應包含以下操作:
+	//  - 根據給定的交易單更新最新狀態(e.g. 已完成, 已取消)
+	//  - 根據給定的撮合紀錄寫入 RDB
+	//
+	// @param ctx context for tracing
+	//
+	// @param orders 欲更新的交易單與交易單狀態
+	//
+	// @param logs 欲寫入的撮合紀錄
 	UpdateOrdersAndCreateTransactionLogs(ctx context.Context, orders map[int]*model.Order, logs []*model.TransactionLog)
 
+	// GetTransactionLogs 取得撮合紀錄
+	// 根據傳入的 filters 組裝 SQL 後取回符合條件的撮合紀錄
+	//
+	// @param ctx context for tracing
+	//
+	// @param opts 查詢條件
+	//
+	// @return []*model.TransactionLog 符合條件的撮合紀錄
 	GetTransactionLogs(ctx context.Context, opts *model.TransactionLogQueryOpts) []*model.TransactionLog
 }
--- practice/interview-20231208/pkg/trading/interface.go
+++ practice/interview-20231208/pkg/trading/interface.go
@@ -7,11 +7,39 @@ import (
 )
 
 type TradingPool interface {
+	// Enable 開始啟動股票撮合模組
+	//
+	// @param ctx context for tracing
 	Enable(ctx context.Context)
 
+	// AddOrder 建立一筆新的掛單
+	//
+	// @param order 欲建立的交易單
+	//
+	// @return e.Code 執行結果
 	AddOrder(order *model.Order) e.CODE
 
+	// GetOrders 取得交易單
+	// 根據傳入的 filters 取回符合條件的交易單
+	//
+	// @param limit 取回的交易單數量上限
+	//
+	// @param opts 查詢條件
+	//
+	// @return []*model.Order 符合條件的交易單
+	//
+	// @return e.Code 執行結果
 	GetOrders(limit int, opts *model.OrderQueryOpts) ([]*model.Order, e.CODE)
 
+	// GetTransactionLogs 取得撮合紀錄
+	// 根據傳入的 filters 取回符合條件的撮合紀錄
+	//
+	// @param limit 取回的撮合紀錄數量上限
+	//
+	// @param opts 查詢條件
+	//
+	// @return []*model.TransactionLog 符合條件的撮合紀錄
+	//
+	// @return e.Code 執行結果
 	GetTransactionLogs(limit int, opts *model.TransactionLogQueryOpts) ([]*model.TransactionLog, e.CODE)
 }
